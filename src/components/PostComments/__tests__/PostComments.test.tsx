ba0ebbd284bf96b19c1dcc1eb50ebd0139fc123d
Huascar Cavalcanti Pinheiro <105388129+HuascarPinheiro@users.noreply.github.com>
2025-07-25 18:57:15 -0300

    Testes de inserçao de 2 comentarios

---
+++ src/components/PostComments/__tests__/PostComments.test.tsx
@@ -0,0 +1,31 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+import PostComments from '..'
+
+describe('Testes para o componente PostComments', () => {
+    test('Deve renderizar corretamente', () => {
+        render(<PostComments />)
+        expect(screen.getByText('Comentar')).toBeInTheDocument()
+    })
+
+    test('Deve adicionar dois comentários', () => {
+        // usei para ver se os comentarios estavam funcionando: const {debug} = 
+        render(<PostComments />)
+
+        const textarea = screen.getByTestId('campo-comentario')
+        const botao = screen.getByTestId('btn-comentar')
+
+        fireEvent.change(textarea, {
+            target: { value: 'Comentário 1' }
+        })
+        fireEvent.click(botao)
+
+        fireEvent.change(textarea, {
+            target: { value: 'Comentário 2' }
+        })
+        fireEvent.click(botao)
+        // debug()
+
+        expect(screen.getByText('Comentário 1')).toBeInTheDocument()
+        expect(screen.getByText('Comentário 2')).toBeInTheDocument()
+    })
+})
